f8c6083e55890a374d8d6d86fc3a93946456f977
Mark Diax <markoffenberg@gmail.com>
2017-11-27 21:12:30 +0100

    Merge is playable

--- Assets/Scripts/Extensions/BaseAnimator.cs
+++ Assets/Scripts/Extensions/BaseAnimator.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BaseAnimator : MonoBehaviour {
-
+	[HideInInspector]
 	public Animator Animator;
 
 	protected virtual void Awake() {
