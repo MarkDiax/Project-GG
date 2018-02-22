94138be24d235aa67a2441dc67cf69a0ab54ecc8
Mark Offenberg <mark-offenberg@hotmail.com>
2018-02-22 17:50:31 +0100

    Updated character
    changed to character controller movement instead of rigidbody

--- Assets/Scripts/Gameplay/Player/Player.cs
+++ Assets/Scripts/Gameplay/Player/Player.cs
@@ -28,4 +28,5 @@ public class Player : MonoBehaviour {
 			return rigidbody;
 		}
 	}
+
 }
--- Assets/Scripts/Gameplay/Player/PlayerController.cs
+++ Assets/Scripts/Gameplay/Player/PlayerController.cs
@@ -7,8 +7,8 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     private Player player;
-    private InputManager Input;
-
+    private InputManager input;
+    private CharacterController character;
     private Transform cameraTransform;
 
     public float walkSpeed = 2;
@@ -23,6 +23,8 @@ public class PlayerController : MonoBehaviour
     private float currentSpeed;
 
     public float jumpVelocity;
+    [Range(0,1)]
+    public float jumpScale;
 
     public bool Grounded;
     Vector3 moveDirection = Vector3.zero;
@@ -30,43 +32,40 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         cameraTransform = Camera.main.transform;
-        Input = InputManager.Instance;
+        input = InputManager.Instance;
         player = PlayerTracker.Player;
+        character = GetComponent<CharacterController>();
     }
 
     private void Update()
     {
-        RaycastGround();
-
         player.Animator.SetBool("Grounded", Grounded);
 
         Move();
     }
 
-    private void RaycastGround()
+    private void OnDrawGizmos()
     {
-        RaycastHit Info;
-        Physics.Raycast(new Ray(transform.position + new Vector3(0, 0.1f, 0), Vector3.down), out Info, 0.2f);
-
-        Grounded = (Info.collider != null);
+        Gizmos.DrawRay(transform.position + new Vector3(0, 0.5f), new Vector3(0,-0.6f));
     }
-
+   
     private void FixedUpdate()
     {
-
+        RaycastHit Hit;
+        Grounded = Physics.Raycast(transform.position + new Vector3(0, 0.5f), Vector3.down, out Hit, 0.6f);
     }
 
     private void Move()
     {
-        Vector2 keyboardInput = Input.Keyboard.Input;
+        Vector2 keyboardInput = input.Keyboard.Input;
         Vector2 inputDir = keyboardInput.normalized;
+
+        Running = UnityEngine.Input.GetAxisRaw("Run") > 0;
 
         if (inputDir != Vector2.zero) {
             float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
             transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
         }
-
-        Running = UnityEngine.Input.GetAxisRaw("Run") > 0;
 
         float targetSpeed = (Running ? runSpeed : walkSpeed) * inputDir.magnitude;
         currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
@@ -74,17 +73,22 @@ public class PlayerController : MonoBehaviour
         float animationSpeed = (Running ? 1 : .5f) * inputDir.magnitude;
         player.Animator.SetFloat("Speed", animationSpeed, speedSmoothTime, Time.deltaTime);
 
-        moveDirection = transform.forward * currentSpeed;
+        moveDirection = transform.forward * currentSpeed + new Vector3(0,moveDirection.y);
 
-        if (UnityEngine.Input.GetKeyDown(KeyCode.Space) && Grounded) {
-            player.Animator.SetTrigger("Jump");
-            player.Rigidbody.AddForce(Vector3.up * jumpVelocity);
+        if (character.isGrounded) {
+
+            if (UnityEngine.Input.GetKeyDown(KeyCode.Space)) {
+                player.Animator.SetTrigger("Jump");
+                //player.Rigidbody.AddForce(Vector3.up * jumpVelocity);
+
+                moveDirection.y = jumpVelocity;
+            }
         }
 
-        transform.Translate(moveDirection * Time.deltaTime, Space.World);
+        moveDirection.y += Physics.gravity.y * jumpScale;
+        character.Move(moveDirection * Time.deltaTime);
+        //transform.Translate(moveDirection * Time.deltaTime, Space.World);
     }
-
-
 
     //private void TiltPlayerWithMouse() {
     //	Vector3 PlayerRot = transform.eulerAngles;
