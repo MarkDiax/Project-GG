49461d91fbf80e81cec9f703be2b3b6c839a8dc3
Mark Offenberg <mark-offenberg@hotmail.com>
2018-03-07 15:57:23 +0100

    PlayerBall.cs serves as rope-climber

--- Assets/Scripts/Gameplay/Player/PlayerBall.cs
+++ Assets/Scripts/Gameplay/Player/PlayerBall.cs
@@ -2,9 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerBall : MonoBehaviour {
+public class PlayerBall : MonoBehaviour
+{
 
-    private void Update()
-    { 
+    private Coroutine Climb;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Rope" && Climb == null)
+            Climb = StartCoroutine(RopeClimb(other));
+    }
+
+    private IEnumerator RopeClimb(Collider Part)
+    {
+
+        CableComponent Cable = Part.gameObject.GetComponentInParent<CableComponent>();
+
+        Collider currentPart = Part;
+        //transform.parent = currentPart.transform;
+
+        int index = int.MaxValue;
+        for (int i = 0; i < Cable.Points.Length; i++)
+        {
+            if (Cable.Points[i] == currentPart)
+                index = i;
+        }
+
+
+        while (index > 0)
+        {
+            if (Vector3.Distance(transform.position, currentPart.gameObject.transform.position) < 0.1f)
+            {
+                index--;
+                currentPart = Cable.Points[index];
+                transform.parent = currentPart.transform;
+            }
+
+            Debug.Log(index);
+
+            transform.position = Vector3.Lerp(transform.position, currentPart.transform.position, 2f * Time.deltaTime);
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        transform.parent = null;
+        transform.position += Vector3.up * 2f;
+        Climb = null;
     }
 }
--- Assets/Scripts/Gameplay/Player/PlayerController.cs
+++ Assets/Scripts/Gameplay/Player/PlayerController.cs
@@ -36,8 +36,6 @@ public class PlayerController : MonoBehaviour
     private Vector3 moveDir;
     private float gravity;
 
-    private Coroutine Climb;
-
     private void Awake()
     {
         cameraTransform = Camera.main.transform;
@@ -111,7 +109,7 @@ public class PlayerController : MonoBehaviour
         float animationSpeed = (Running ? currentSpeed / runSpeed : currentSpeed / walkSpeed * 0.5f) * inputDir.magnitude;
         player.Animator.SetFloat("Speed", animationSpeed, speedSmoothTime, Time.deltaTime);
 
-        player.Animator.SetFloat("VelocityY", controller.velocity.y / gravity);
+        //player.Animator.SetFloat("VelocityY", controller.velocity.y / gravity);
     }
 
     private void Jump()
@@ -135,51 +133,6 @@ public class PlayerController : MonoBehaviour
 
         return smoothTime / airControl;
     }
-
-
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.tag == "Rope" && Climb == null)
-    //        Climb = StartCoroutine(RopeClimb(other));
-
-    //}
-
-    private IEnumerator RopeClimb(Collider Part)
-    {
-
-        CableComponent Cable = Part.gameObject.GetComponentInParent<CableComponent>();
-
-        Collider currentPart = Part;
-        transform.parent = currentPart.transform;
-
-        int index = int.MaxValue;
-        for (int i = 0; i < Cable.Points.Length; i++)
-        {
-            if (Cable.Points[i] == currentPart)
-                index = i;
-        }
-
-
-        while (index > 0)
-        {
-            if (Vector3.Distance(transform.position, currentPart.gameObject.transform.position) < 0.1f)
-            {
-                index--;
-                currentPart = Cable.Points[index];
-                transform.parent = currentPart.transform;
-            }
-
-            Debug.Log(index);
-
-            transform.position = Vector3.Lerp(transform.position, currentPart.transform.position, 2f * Time.deltaTime);
-
-            yield return new WaitForFixedUpdate();
-        }
-
-        transform.parent = null;
-    }
-
-
 
     //private void TiltPlayerWithMouse() {
     //	Vector3 PlayerRot = transform.eulerAngles;
