d1d31eb3fea7a2f41971c3e0c615f532088c763a
Mark Offenberg <mark-offenberg@hotmail.com>
2018-03-07 15:41:38 +0100

    Test scene for rope

---
+++ Assets/Scripts/Gameplay/Player/PlayerBall.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBall : MonoBehaviour {
+
+    private void Update()
+    { 
+    }
+}
--- Assets/Scripts/Gameplay/Player/PlayerController.cs
+++ Assets/Scripts/Gameplay/Player/PlayerController.cs
@@ -36,6 +36,8 @@ public class PlayerController : MonoBehaviour
     private Vector3 moveDir;
     private float gravity;
 
+    private Coroutine Climb;
+
     private void Awake()
     {
         cameraTransform = Camera.main.transform;
@@ -91,7 +93,7 @@ public class PlayerController : MonoBehaviour
 
             //z-tilt
             float zOffset = moveVector.y - previousY;
-            Vector3 tiltVector = Vector3.forward * Mathf.SmoothDampAngle(transform.eulerAngles.z, -zOffset * 1.6f, ref tiltSmoothVelocity, GetModifiedSmoothTime(tiltSmoothTime));
+            Vector3 tiltVector = Vector3.forward * Mathf.SmoothDampAngle(transform.eulerAngles.z, -zOffset * 1.2f, ref tiltSmoothVelocity, GetModifiedSmoothTime(tiltSmoothTime));
 
             transform.eulerAngles = moveVector + tiltVector;
         }
@@ -133,6 +135,51 @@ public class PlayerController : MonoBehaviour
 
         return smoothTime / airControl;
     }
+
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.tag == "Rope" && Climb == null)
+    //        Climb = StartCoroutine(RopeClimb(other));
+
+    //}
+
+    private IEnumerator RopeClimb(Collider Part)
+    {
+
+        CableComponent Cable = Part.gameObject.GetComponentInParent<CableComponent>();
+
+        Collider currentPart = Part;
+        transform.parent = currentPart.transform;
+
+        int index = int.MaxValue;
+        for (int i = 0; i < Cable.Points.Length; i++)
+        {
+            if (Cable.Points[i] == currentPart)
+                index = i;
+        }
+
+
+        while (index > 0)
+        {
+            if (Vector3.Distance(transform.position, currentPart.gameObject.transform.position) < 0.1f)
+            {
+                index--;
+                currentPart = Cable.Points[index];
+                transform.parent = currentPart.transform;
+            }
+
+            Debug.Log(index);
+
+            transform.position = Vector3.Lerp(transform.position, currentPart.transform.position, 2f * Time.deltaTime);
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        transform.parent = null;
+    }
+
+
 
     //private void TiltPlayerWithMouse() {
     //	Vector3 PlayerRot = transform.eulerAngles;
