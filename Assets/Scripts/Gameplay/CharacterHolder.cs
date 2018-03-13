eb21161f71b32f6e75ed8b0f64223f06f058d6de
Noah_Artwork <noahkobus@hotmail.com>
2018-03-13 22:36:31 +0100

    Moving Platform
    Still needs to inheret movement of platform when jumping off.

---
+++ Assets/Scripts/Gameplay/CharacterHolder.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterHolder : MonoBehaviour
+{
+    public bool IsExitTrigger;
+       
+    private void OnTriggerEnter(Collider other)
+    {
+        if(IsExitTrigger == false)
+        {
+            other.transform.parent = gameObject.transform;
+        }
+       
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(IsExitTrigger == true)
+        {
+            other.transform.parent = null;
+        }
+    }
+
+}
