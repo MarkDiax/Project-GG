7ad47c2c5185935c0c8e259f3f40072cc0427fd6
Noah_Artwork <noahkobus@hotmail.com>
2018-03-21 13:11:16 +0100

    Level Update + Heatmap

---
+++ Assets/Scripts/Gameplay/MoveObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour {
+
+    public float MinClamp = 30f;
+    public float MaxClamp = 150f;
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.eulerAngles = new Vector3(Mathf.Clamp(transform.rotation.eulerAngles.x, MinClamp, MaxClamp),transform.rotation.eulerAngles.y, Mathf.Clamp(transform.rotation.eulerAngles.z, MinClamp, MaxClamp));
+    }
+}
---
+++ Assets/Scripts/Gameplay/TeleportBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportBehaviour : MonoBehaviour {
+
+    public GameObject endPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.transform.position = endPoint.transform.position;
+    }
+}
