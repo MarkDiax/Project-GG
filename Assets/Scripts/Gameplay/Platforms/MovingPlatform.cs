8bd54713a985e18d1e8dfaf8128cee55ad674541
Noah_Artwork <noahkobus@hotmail.com>
2018-04-03 14:36:44 +0200

    Tutorial Levels 0.02

---
+++ Assets/Scripts/Gameplay/Platforms/MovingPlatform.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+
+    [SerializeField]
+    public Transform platform;
+
+    public Transform startPosition;
+    public Transform endPosition;
+
+    public  float platformSpeed;
+    private Vector3 direction;
+    Transform destination;
+
+	// Use this for initialization
+	void Start () {
+        SetDestination(startPosition);
+	}
+	
+
+	void FixedUpdate () {
+        platform.transform.Translate(direction * platformSpeed * Time.fixedDeltaTime, Space.World);
+
+        if(Vector3.Distance (platform.position, destination.position) < platformSpeed * Time.fixedDeltaTime)
+        {
+            SetDestination(destination == startPosition ? endPosition : startPosition);
+        }
+	}
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(startPosition.position, startPosition.localScale);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(endPosition.position, endPosition.localScale);
+
+    }
+
+    void SetDestination(Transform dest)
+    {
+        destination = dest;
+        direction = (destination.position - platform.position).normalized;
+    }
+}
